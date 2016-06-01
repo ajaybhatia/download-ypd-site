effcaa818b115130b174d5a2a417823c5476173c
ajaybhatia <prof.ajaybhatia@gmail.com>
2016-06-01 23:47:58 +0530

    Changes to overall UI

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,344 @@
-$(document).ready(function() {
-}); 
+window.scrollYLast = 0;
+window.animScrollToY = function (targetY, callback) {
+    window.scrollYLast = window.scrollY;
+
+    targetY -= 60; // skip the header overlay.
+    if (targetY < 0) targetY = 0; // oops.
+
+    if (targetY === window.scrollY) {
+        // we are already done. real helpful when the user is at the top and we want to go to Y=0.
+        callback();
+        return;
+    }
+
+    $("html, body").stop().animate({ scrollTop: targetY }, 800, 'swing').promise().done(callback);
+};
+
+function hasClass(original, className) {
+    return (!!original) ? (!!original.match('(^|^.* )' + className + '( .*$|$)')) : (false);
+}
+
+function addClass(original, className) {
+    if (!original) return className.split('  ').join(' ').trim();
+    var m = original.match('(^|^.* )' + className + '( .*$|$)');
+    return ((!!m) ? (original) : (original + ' ' + className)).split('  ').join(' ').trim();
+}
+
+function removeClass(original, className) {
+    if (!original) return '';
+    var m = original.match('(^|^.* )' + className + '( .*$|$)');
+    return ((!!m) ? ((m[1] || '') + (m[2] || '')) : (original)).split('  ').join(' ').trim();
+}
+
+function toggleClass(original, className) {
+    if (!original) return className.split('  ').join(' ').trim();
+    var m = original.match('(^|^.* )' + className + '( .*$|$)');
+    return ((!!m) ? ((m[1] || '') + (m[2] || '')) : (original + ' ' + className)).split('  ').join(' ').trim();
+}
+
+function closeDrawer() {
+    var drawer = document.getElementById('nav-drawer');
+    if (!!drawer) drawer.className = removeClass(drawer.className, 'visible');
+
+    document.body.className = removeClass(document.body.className, 'js-nav-drawer');
+
+    var hamburger = document.getElementById('header-menu-button');
+    if (!!hamburger) hamburger.className = removeClass(hamburger.className, 'pressed');
+
+
+    setTimeout(function () {
+        window.animScrollToY(window.scrollYLast, function () {});
+    }, 1000);
+}
+
+function openDrawer() {
+    window.animScrollToY(0, function () {
+        var drawer = document.getElementById('nav-drawer');
+        if (!!drawer) drawer.className = addClass(drawer.className, 'visible');
+
+        document.body.className = addClass(document.body.className, 'js-nav-drawer');
+
+        var hamburger = document.getElementById('header-menu-button');
+        if (!!hamburger) hamburger.className = addClass(hamburger.className, 'pressed');
+    });
+}
+
+function toggleDrawer(event) {
+    var drawer = document.getElementById('nav-drawer');
+    if (!drawer) return;
+
+    event.preventDefault();
+
+    if (hasClass(drawer.className, 'visible')) {
+        closeDrawer();
+    } else {
+        openDrawer();
+    }
+}
+
+var lastVisibleId = '';
+function updateLocation(doSwitch, targetName) {
+    var sections = document.getElementsByClassName('site-section');
+    for (var i = 0; i < sections.length; i++) {
+        var section = sections[i];
+        if (hasClass(section.className, 'visible')) lastVisibleId = section.id.substr('section-'.length);
+        if (doSwitch && !hasClass(section.className, 'white')) section.className = addClass(removeClass(section.className, 'visible'), 'hidden');
+    }
+
+    var pathParts = location.pathname.split('/');
+    var lastPart = pathParts.pop().split('.')[0] || 'index';
+    var sectionName = ((!!targetName) ? (targetName.split('/').pop()) : (lastPart)) || 'index';
+
+    var target = document.getElementById('section-' + sectionName);
+    if (!!target) {
+        if (doSwitch) {
+            target.className = addClass(removeClass(target.className, 'hidden'), 'visible');
+            if (sectionName !== lastPart) {
+                window.history.pushState({}, 'Downloads | YU Play Dev', pathParts.join('/') + '/' + (sectionName === 'index' ? '' : sectionName));
+                hasLocationUpdates = true;
+            }
+            window.scrollYLast = 0;
+            closeDrawer();
+        }
+    } else {
+        var client = new XMLHttpRequest();
+        client.onreadystatechange = function () {
+            if (client.readyState !== 4) return; // not complete
+
+            var foobar = document.createElement('div');
+            foobar.innerHTML = client.responseText;
+            var newSections = foobar.getElementsByClassName('site-section');
+            if (newSections.length > 0) {
+                var node = document.importNode(newSections[0], true);
+                node.className = addClass(removeClass(node.className, 'visible'), 'hidden');
+                var existingNode = document.getElementById(node.id);
+                if (!!existingNode) existingNode.parentElement.removeChild(existingNode);
+                sections[0].parentNode.appendChild(node);
+                setUpSections();
+            }
+            setTimeout(function () {
+                if (doSwitch) {
+                    target = document.getElementById('section-' + sectionName);
+                    if (!!target) {
+                        target.className = addClass(removeClass(target.className, 'hidden'), 'visible');
+                    } else {
+                        sectionName = lastVisibleId;
+                        target = document.getElementById('section-' + sectionName);
+                        if (!!target) target.className = addClass(removeClass(target.className, 'hidden'), 'visible');
+                    }
+                    if (sectionName !== lastPart) {
+                        window.history.pushState({}, 'Paranoid Android – Official', pathParts.join('/') + '/' + (sectionName === 'index' ? '' : sectionName));
+                        hasLocationUpdates = true;
+                    }
+                    window.scrollYLast = 0;
+                    closeDrawer();
+                }
+            }, 1);
+        };
+        client.open('GET', '/' + sectionName + '.html', true);
+        client.send();
+        // TODO use a spinner
+    }
+}
+
+// google analytics
+var hasLocationUpdates = true;
+setInterval(function () {
+    if (!hasLocationUpdates) return;
+    ga('send', 'pageview');
+    hasLocationUpdates = false;
+}, 1000);
+
+function pressNavigationLink(event) {
+    event.preventDefault();
+    updateLocation(true, event.target.href);
+    closeDrawer();
+}
+
+function getRecursiveTopOffset(element) {
+    var topOffset = 0;
+
+    while (element.offsetParent !== undefined && element.offsetParent !== null) {
+        topOffset += element.offsetTop + (element.clientTop === null ? 0 : element.clientTop);
+        element = element.offsetParent || {};
+    }
+
+    return topOffset;
+}
+
+function pressDownArrow(event) {
+    event.preventDefault();
+
+    var downarrow = document.getElementById('downarrow');
+    if (!downarrow) return; // what the.
+
+    var sectionScroll = downarrow.getAttribute('data-scroll-' + (location.pathname.split('/').pop().split('.')[0] || 'index'));
+    if (!sectionScroll) return; // what the.
+
+    var target = document.getElementById(sectionScroll);
+    if (!target) return; // what the.
+
+    window.animScrollToY(getRecursiveTopOffset(target), function () {});
+}
+
+var hasMoved = false;
+function updateScroll(event) { hasMoved = true; }
+setInterval(function () {
+    if (!hasMoved) return;
+
+    var downarrow = document.getElementById('downarrow');
+    if (!!downarrow) {
+        var isHidden = hasClass(downarrow.className, 'hidden');
+        var hasSectionScroll = !!(downarrow.getAttribute('data-scroll-' + (location.pathname.split('/').pop().split('.')[0] || 'index')));
+        if (window.scrollY > 100 || !hasSectionScroll) {
+            if (!isHidden) {
+                downarrow.className = addClass(downarrow.className, 'hidden');
+            }
+        } else {
+            if (isHidden) {
+                downarrow.className = removeClass(downarrow.className, 'hidden');
+            }
+        }
+    }
+
+    var targets = document.getElementsByClassName('js-animate-viewport');
+    for (var i = 0; i < targets.length; i++) {
+        var target = targets[i];
+        var rect = target.getBoundingClientRect();
+        var isAnimated = hasClass(target.className, 'animate');
+        if (!isAnimated) {
+            if (rect.top <= window.innerHeight && rect.left <= window.innerWidth) {
+                target.className = addClass(target.className, 'animate');
+            }
+        } else {
+            if (rect.left > window.innerWidth || rect.right < 0) {
+                target.className = removeClass(target.className, 'animate');
+            }
+        }
+    }
+
+    hasMoved = false;
+}, 1000 / 60);
+
+function updatePopstate(event) { updateLocation(true); }
+
+function setValue(element, className, value, modifiableField) {
+    if (hasClass(element.className, className)) {
+        if (!modifiableField) {
+            var tag = element.tagName.toLowerCase();
+            if (tag === 'img') return true; // abort
+            if (tag === 'a' && value.substr(0, 4) === 'http') modifiableField = 'href';
+            else modifiableField = 'innerHTML';
+        }
+        element[modifiableField] = value;
+        return true;
+    }
+
+    var children = element.children;
+    for (var child in children) {
+        if (!children.hasOwnProperty(child)) continue;
+        if (setValue(children[child], className, value, modifiableField)) return true;
+    }
+
+    return false;
+}
+
+function setUpSections() {
+    var noscripts = document.getElementsByClassName('noscript');
+    while (noscripts.length > 0) {
+        for (var nsi = 0; nsi < noscripts.length; nsi++) {
+            var noscript = noscripts[nsi];
+            noscript.className = removeClass(noscript.className, 'noscript');
+        }
+
+        noscripts = document.getElementsByClassName('noscript');
+    }
+
+    var navigationLinks = document.getElementsByClassName('js-navigationlink');
+    while (navigationLinks.length > 0) {
+        for (var navi = 0; navi < navigationLinks.length; navi++) {
+            var link = navigationLinks[navi];
+            link.onclick = pressNavigationLink;
+            link.className = removeClass(link.className, 'js-navigationlink');
+        }
+
+        navigationLinks = document.getElementsByClassName('js-navigationlink');
+    }
+
+    var memberSample = document.getElementById('team-member-sample');
+    if (!!memberSample) {
+        for (var person in people) {
+            if (!people.hasOwnProperty(person)) continue;
+            var info = people[person];
+
+            var member = memberSample.cloneNode(true);
+            member.className = removeClass(member.className, 'sample');
+            member.id = '';
+
+            for (var field in info) {
+                if (!info.hasOwnProperty(field)) continue;
+                setValue(member, field, info[field]);
+            }
+
+            setValue(member, 'name', person, 'innerHTML');
+            setValue(member, 'profile', info.profile || '', 'src');
+
+            memberSample.parentElement.appendChild(member);
+        }
+        memberSample.parentElement.removeChild(memberSample);
+    }
+}
+
+$(document).ready(function () {
+    var hamburger = document.getElementById('header-menu-button');
+    if (!!hamburger) hamburger.onclick = toggleDrawer;
+
+    var downarrow = document.getElementById('downarrow');
+    if (!!downarrow) { downarrow.onclick = pressDownArrow; }
+
+    setInterval(function () {
+        var carousels = document.getElementsByClassName('carousel');
+        for (var i = 0; i < carousels.length; i++) {
+            var carousel = carousels[i];
+            var images = carousel.children;
+
+            // do nothing if the user has scrolled beyond the carousel
+            if (window.scrollY > getRecursiveTopOffset(carousel) + jQuery(carousel).height() - 60) continue;
+
+            var imageHidden = false;
+            var imageDisplayed = false;
+
+            for (var j = 0; j < images.length; j++) {
+                if (imageDisplayed) break;
+
+                var image = images[j];
+
+                if (imageHidden) {
+                    image.className = addClass(image.className, 'visible');
+                    imageDisplayed = true;
+                } else if (hasClass(image.className, 'visible')) {
+                    image.className = removeClass(image.className, 'visible');
+                    imageHidden = true;
+                }
+            }
+
+            if (!imageDisplayed && images.length > 0) {
+                var firstImage = images[0];
+                firstImage.className = addClass(firstImage.className, 'visible');
+                imageDisplayed = true;
+            }
+        }
+    }, 5000);
+
+    window.onscroll = updateScroll;
+    window.onpopstate = updatePopstate;
+
+    // force updates
+    updateLocation(true);
+    setUpSections();
+    hasMoved = true;
+    setTimeout(function () { hasMoved = true; }, 1500);
+    setTimeout(function () { hasMoved = true; }, 3000);
+});
+
+// google analytics
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,79 +1,155 @@
 <!DOCTYPE html>
-<html lang="en-US">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>YU Play Dev | Downloads</title>
-
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
-        <!-- Fontawesome CSS -->
-        <link rel="stylesheet" href="{{ URL::asset('css/font-awesome.min.css') }}">
-        <!-- Custom CSS -->
-        <link rel="stylesheet" href="{{ URL::asset('css/styles.css') }}">
-
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
-            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
-    </head>
-    <body>
-        <nav class="navbar navbar-default" role="navigation">
-            <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">YU Play Dev</a>
-                </div>
-        
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse navbar-ex1-collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Downloads</a></li>
-                    </ul>
-                    <form class="navbar-form navbar-right" role="search">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Search">
-                        </div>
-                        <button type="submit" class="btn btn-default">Submit</button>
-                    </form>
-                    <!--
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">Link</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li><a href="#">Separated link</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    -->
-                </div><!-- /.navbar-collapse -->
+<html dir="ltr" lang="en-US">
+   <head>
+      <meta charset="utf-8">
+      <!-- Main information -->
+      <title>Downloads | YU Play Dev</title>
+      <link rel="shortcut icon" href="">
+      <link rel="icon" sizes="192x192" href="">
+      <link rel="apple-touch-icon" href="">
+      <link rel="apple-touch-icon-precomposed" href="">
+      <!-- HTML5 shim -->
+      <!--[if lt IE 9]>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <![endif]-->
+      <!-- Meta -->
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+      <meta name="apple-mobile-web-app-capable" content="yes">
+      <meta name="mobile-web-app-capable" content="yes">
+      <meta name="theme-color" content="#4C4C4C">
+      <!-- Google Fonts -->
+      <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Montserrat:400,700">
+      <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Raleway:500">
+      <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,300italic,400,400italic,500,500italic,700,700italic">
+      <!-- Fontawesome CSS -->
+      <link rel="stylesheet" href="{{ URL::asset('css/font-awesome.min.css') }}">
+      <!-- Custom style -->
+      <link rel="stylesheet" href="{{ URL::asset('css/styles.css') }}">
+   </head>
+   <body class="noscript">
+      <!-- Header -->
+      <header class="desktop">
+         <div class="header-button">
+            <p><a class="js-navigationlink" href="#">YU Play Dev</a></pa>
+         </div>
+         <div class="header-button">
+            <p><a class="js-navigationlink" href="#">Downloads</a></p>
+         </div>
+      </header>
+      <header class="mobile">
+         <div class="header-button">
+            <p><a class="js-navigationlink" href="index.html">YU Play Dev</a></pa>
+         </div>
+      </header>
+      <button id="header-menu-button"><span></span><span></span><span></span></button><!-- Navigation drawer -->
+      <section id="nav-drawer">
+         <div class="nav-side">
+            <ul class="nav-side-central">
+               <li>
+                  <p><a class="js-navigationlink" href="index.html">Home</a></p>
+               </li>
+               <li>
+                  <p><a href="">Downloads</a></p>
+               </li>
+               <li>
+                  <p><a class="js-navigationlink" href=""></a></p>
+               </li>
+               <li><span class="separator"></span></li>
+               <li>
+                  <p><a href="">JIRA</a></p>
+               </li>
+               <li>
+                  <p><a href="">Gerrit</a></p>
+               </li>
+               <li>
+                  <p><a href="https://github.com/YUTeleventures">GitHub</a></p>
+               </li>
+               <li class="desktop">
+                  <p>Some Message</p>
+               </li>
+            </ul>
+         </div>
+         <div class="nav-side">
+            <div class="nav-side-central">
+               <p><a class="js-navigationlink" href="index.html">Some YU Icon</a></p>
             </div>
-        </nav>
+         </div>
+      </section>
+      <!-- The noscript notice -->
+      <noscript class="notice">
+         <div class="notice-central">
+            <img class="animate" src="" alt="Some YU Icon">
+            <p>Hey! This site depends on JavaScript like many others.<br>Please enable your JS and reload this page.</p>
+         </div>
+      </noscript>
+      <!-- Site section -->
+      <section id="section-index" class="site-section visible">
+         <!-- Promotional images -->
+         <div class="carousel">
+            <img class="visible" src="">
+            <img src="">
+            <img src=""></div>
+         <!-- Introduction text -->
+         <div id="intro">
+            <img class="js-animate-viewport" src="">
+            <p>Introduction</p>
+            <span class="separator"></span>
+         </div>
+         <!-- Feature highlights -->
+         <div id="features">
+            <div class="feature">
+               <img class="demo js-animate-viewport" src="">
+               <div class="text">
+                  <p class="title">Title 1</p>
+                  <p class="description">
+                    Description 1
+                </p>
+               </div>
+            </div>
+            <div class="feature">
+               <img class="demo js-animate-viewport" src="">
+               <div class="text">
+                  <p class="title">Title 2</p>
+                  <p class="description">
+                      Description 2
+                  </p>
+               </div>
+            </div>
+            <div class="feature">
+               <img class="demo js-animate-viewport" src="">
+               <div class="text">
+                  <p class="title">Title 3</p>
+                  <p class="description">
+                      Description 3
+                  </p>
+               </div>
+            </div>
+            <div class="feature">
+               <img class="demo js-animate-viewport" src="">
+               <div class="text">
+                  <p class="title">Title 4</p>
+                  <p class="description">
+                      Description 4
+                  </p>
+               </div>
+            </div>
+            <div class="nofloat"></div>
+         </div>
+         <span class="separator"></span>
+         <!-- Footer quote -->
+         <footer>
+            <p>YU Televentures</p>
+         </footer>
+      </section>
+      <!-- White placeholder screen for the initial animation -->
+      <section class="site-section white"></section>
+      <!-- Down arrow --> 
+      <button id="downarrow" data-scroll-index="intro">
+        <img class="rot180" src="{{ URL::asset('images/arrow.svg') }}" alt="Down arrow">
+      </button>
 
-        <div class="container main">
-            @yield('content')
-        </div>
-
-        <!-- jQuery -->
-        <script src="{{ URL::asset('js/jquery-2.2.4.min.js') }}"></script>
-        <!-- Bootstrap JavaScript -->
-        <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
-        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-        <script src="Hello World"></script>
-        <!-- Custom js -->
-        <script src="{{ URL::asset('js/main.js') }}"></script>
-    </body>
+      <!-- JavaScript -->
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+      <script src="{{ URL::asset('js/main.js') }}"></script>
+   </body>
 </html>
