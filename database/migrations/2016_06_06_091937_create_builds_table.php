055161ba4bd92f4ee824315dda55d94ec22491ff
ajaybhatia <prof.ajaybhatia@gmail.com>
2016-06-06 15:01:51 +0530

    Migration for "Build" model

---
+++ database/migrations/2016_06_06_091937_create_builds_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBuildsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('builds', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('device_name');
+            $table->string('build_type');
+            $table->string('build_name');
+            $table->string('sha1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('builds');
+    }
+}
