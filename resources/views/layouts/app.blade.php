c0b79e3dd73d1512772ec096983b32e74b49f010
ajaybhatia <prof.ajaybhatia@gmail.com>
2016-06-06 15:36:22 +0530

    Auth initiated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,100 +1,82 @@
 <!DOCTYPE html>
-<html dir="ltr" lang="en-US">
-   <head>
-      <meta charset="utf-8">
-      <!-- Main information -->
-      <title>Downloads | YU Play Dev</title>
-      <link rel="shortcut icon" href="">
-      <link rel="icon" sizes="16x16" href="{{ URL::asset('images/favicon.ico') }}">
-      <link rel="apple-touch-icon" href="">
-      <link rel="apple-touch-icon-precomposed" href="">
-      <!-- HTML5 shim -->
-      <!--[if lt IE 9]>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <![endif]-->
-      <!-- Meta -->
-      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-      <meta name="apple-mobile-web-app-capable" content="yes">
-      <meta name="mobile-web-app-capable" content="yes">
-      <meta name="theme-color" content="#4C4C4C">
-      <!-- Google Fonts -->
-      <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Montserrat:400,700">
-      <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Raleway:500">
-      <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,300italic,400,400italic,500,500italic,700,700italic">
-      <!-- Bootstrap CSS -->
-      <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
-      <!-- Fontawesome CSS -->
-      <link rel="stylesheet" href="{{ URL::asset('css/font-awesome.min.css') }}">
-      <!-- Custom style -->
-      <link rel="stylesheet" href="{{ URL::asset('css/styles.css') }}">
-   </head>
-   <body class="noscript">
-      <!-- Header -->
-      <header class="desktop">
-         <div class="header-button">
-            <p><a class="js-navigationlink" href="#">YU Play Dev</a></pa>
-         </div>
-      </header>
-      <header class="mobile">
-         <div class="header-button">
-            <p><a class="js-navigationlink" href="index.html">YU Play Dev</a></pa>
-         </div>
-      </header>
-      <button id="header-menu-button"><span></span><span></span><span></span></button><!-- Navigation drawer -->
-      <section id="nav-drawer">
-         <div class="nav-side">
-            <ul class="nav-side-central">
-               <li>
-                  <p><a class="js-navigationlink" href="#">Home</a></p>
-               </li>
-               <li>
-                  <p><a href="">YU Developers Programme</a></p>
-               </li>
-               <li>
-                  <p><a class="js-navigationlink" href="">YU Forums</a></p>
-               </li>
-               <li><span class="separator"></span></li>
-               <li>
-                  <p><a href="">Build</a></p>
-               </li>
-               <li>
-                  <p><a href="https://github.com/YUTeleventures">GitHub</a></p>
-               </li>
-               <li class="desktop">
-                  <p>Some Message</p>
-               </li>
-            </ul>
-         </div>
-         <div class="nav-side">
-            <div class="nav-side-central">
-                <p>
-                    <a class="js-navigationlink" href="index.html">
-                        <img src="{{ URL::asset('images/yu_play_god_logo.jpg') }}" alt="">
-                    </a>
-                </p>
-            </div>
-         </div>
-      </section>
-      <!-- The noscript notice -->
-      <noscript class="notice">
-         <div class="notice-central">
-            <img class="animate" src="" alt="Some YU Icon">
-            <p>Hey! This site depends on JavaScript like many others.<br>Please enable your JS and reload this page.</p>
-         </div>
-      </noscript>
-      <!-- Site section -->
-      <section id="section-index" class="site-section visible">
-        @yield('content')
-      </section>
-      <!-- White placeholder screen for the initial animation -->
-      <section class="site-section white"></section>
-      <!-- Down arrow --> 
-      <button id="downarrow" data-scroll-index="intro">
-        <img class="rot180" src="{{ URL::asset('images/arrow.svg') }}" alt="Down arrow">
-      </button>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-      <!-- JavaScript -->
-      <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-      <script src="{{ URL::asset('js/main.js') }}"></script>
-   </body>
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+
+    <style>
+        body {
+            font-family: 'Lato';
+        }
+
+        .fa-btn {
+            margin-right: 6px;
+        }
+    </style>
+</head>
+<body id="app-layout">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    Laravel
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ url('/home') }}">Home</a></li>
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    @yield('content')
+
+    <!-- JavaScripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+</body>
 </html>
