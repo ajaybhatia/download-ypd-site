5d60ec1683c937b5bcf7b46fba013028cd7323ce
ajaybhatia <prof.ajaybhatia@gmail.com>
2016-06-06 15:30:53 +0530

    Build_path added to Build migration

--- database/migrations/2016_06_06_091937_create_builds_table.php
+++ database/migrations/2016_06_06_091937_create_builds_table.php
@@ -17,6 +17,7 @@ class CreateBuildsTable extends Migration
             $table->string('device_name');
             $table->string('build_type');
             $table->string('build_name');
+            $table->string('build_path');
             $table->string('sha1');
             $table->timestamps();
         });
